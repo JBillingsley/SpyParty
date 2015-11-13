0e4c2561d8458bcdb7d98617b0d71108854a63c0
John Billingsley <jmb330@case.edu>
2015-11-13 15:54:33 -0800

    Added Main Scene
    We have a game!

---
+++ SpyParty/SpyParty/Assets/Scripts/ClickObject.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof (Collider))]
+public class ClickObject : MonoBehaviour {
+    private Vector3 initialPosition;
+    private bool highlighted = false;
+
+    public GameObject TextPanel;
+
+    void Start() {
+        GetComponent<Renderer>().material.color = Color.white;
+        initialPosition = transform.position;
+    }
+
+	void OnMouseDown() {
+        if(GetComponent<Renderer>().material.color.Equals(Color.white)) {
+            GetComponent<Renderer>().material.color = Color.black;
+        } else {
+            GetComponent<Renderer>().material.color = Color.white;
+        }
+    }
+
+    void OnMouseOver() {
+        highlighted = true;
+    }
+
+    void Update() {
+        if(highlighted) {
+            transform.position = new Vector3(initialPosition.x, initialPosition.y, initialPosition.z - 1f);
+            TextPanel.SetActive(true);
+            highlighted = false;
+        } else {
+            transform.position = initialPosition;
+            TextPanel.SetActive(false);
+        }
+    }
+}

c2e752e0c1976ceffecdbb0e47d7f05a28083c7e
John Billingsley <jmb330@case.edu>
2015-11-27 21:49:03 -0800

    Improved AI pathing

--- SpyParty/SpyParty/Assets/Scripts/NPC.cs
+++ SpyParty/SpyParty/Assets/Scripts/NPC.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
     
     public enum AIStates { WANDER, IDLE, PERSUE };
 
@@ -13,18 +14,27 @@ public class NPC : MonoBehaviour {
     private GameObject goalPoint;
     private AIStates state;
     private float animationTime = 0.6f;
-    private int TurnsWandering = 0;
+    private List<PathNode> pathToGoal;
+    private List<PathNode> openSet = new List<PathNode>(); // set of nodes to be evaluated
+    private List<PathNode> closedSet = new List<PathNode>(); // nodes along the best path
 
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+
+    void Start () {
         textPanel.SetActive(false);
         updateLocation();
         if(board == null) {
             board = GameObject.Find("Board");
         }
-	    if(goalPoint == null) {
+        if(goalPoint == null) {
+           // Debug.Log("goal being set");
+
+            goalPoint = board.GetComponentsInChildren<ClickObject>()[Random.Range(0, board.GetComponentsInChildren<ClickObject>().Length)].gameObject;
+          //  Debug.Log(string.Format("the goalpoint name is {0}", goalPoint));
+            findPath();
             state = AIStates.IDLE;
-           // state = (AIStates)Random.Range(0, 3);
+            // state = (AIStates)Random.Range(0, 3);
         }
         Player.turn += StateMachine;
 
@@ -40,18 +50,22 @@ public class NPC : MonoBehaviour {
         checkRoomInvasion();
         switch(state) {
             case AIStates.WANDER:
-                TurnsWandering += 1;
-                // this means the ai needs a place to go, this happens on a new board or when the ai gets stuck looping for too long
-                if(goalPoint == null || TurnsWandering > 5) {
-                    // This line sucks but it chooses a random square on the board
-                    goalPoint = board.GetComponentsInChildren<ClickObject>()[Random.Range(0, board.GetComponentsInChildren<ClickObject>().Length)].gameObject;
-                    TurnsWandering = 0;
-                } else if(currentSquare.Equals(goalPoint)) {
-                    // you done! 
-                    // choose a new state and return
+                if(goalPoint == null) {
                     selectRandomState();
+                    goalPoint = board.GetComponentsInChildren<ClickObject>()[Random.Range(0, board.GetComponentsInChildren<ClickObject>().Length)].gameObject;
                     return;
                 }
+                if(currentSquare.Equals(goalPoint)) {
+                    // you done! 
+                    // choose a new state and return
+               //     Debug.Log("we have moved into the goal square so we are calculating a new square to wander to");
+                    selectRandomState();
+                    goalPoint = board.GetComponentsInChildren<ClickObject>()[Random.Range(0, board.GetComponentsInChildren<ClickObject>().Length)].gameObject;
+                    cleanPathNodes();
+                    findPath();
+              //      Debug.Log(goalPoint);
+                    return;
+                } 
                 // sets the current square to not holding this character anymore in anticipation for the move
                 // then choose a square that advances the ai towards the target square
                 
@@ -59,11 +73,12 @@ public class NPC : MonoBehaviour {
                 updateLocation();
                 break;
             case AIStates.PERSUE:
-                TurnsWandering = 0;
                 foreach(GameObject square in persueRoom) {
                     if(squareContainsPlayer(square)) {
                         // we do a persuit 
                         goalPoint = square;
+                        cleanPathNodes();
+                        findPath();
                         currentSquare = findNextMove();
                         updateLocation();
                         return;
@@ -74,7 +89,6 @@ public class NPC : MonoBehaviour {
                // selectRandomState();
                 break;
             case AIStates.IDLE:
-                TurnsWandering = 0;
                 state = AIStates.WANDER;
                 goalPoint = null;
                 break;
@@ -84,30 +98,32 @@ public class NPC : MonoBehaviour {
     }
 
     private GameObject findNextMove() {
-        currentSquare.GetComponent<ClickObject>().setCharacter(null);
-        if(goalPoint != null) {
-            float distance = 1000000f;
-           GameObject potentialSquare = null;
-            // this loop finds a square in the direction of the goal point
-            foreach(GameObject square in currentSquare.GetComponent<ClickObject>().neighborCubes) {
-                // the second half of this is a conditional hack so NPCs dont end up in the same square
-                if(Mathf.Abs(Vector3.Distance(square.transform.position, goalPoint.transform.position)) < distance && squareDoesntContainNPC(square)) {
-                    distance = Mathf.Abs(Vector3.Distance(square.transform.position, goalPoint.transform.position));
-                    // if the square contains the player
-                    if(squareContainsPlayer(square)) {
-                        // if it is dangerous it can be chosen 
-                        if(square.GetComponent<ClickObject>().isSquareDangerous()) {
-                            potentialSquare = square;
-                        }
-                        // if it doesnt contain the player it can be chosen
-                    } else {
-                        potentialSquare = square;
-                    }
-                }
-            }
-            return potentialSquare;
+        GameObject potentialSquare = new GameObject();
+        potentialSquare.tag = "Finish";
+        /*
+        if(pathToGoal == null) {
+            Debug.Log("finding a new path");
+            findPath();
         }
-        return null;
+        */
+        if(pathToGoal.Count <= 0) {
+          //  goalPoint = board.GetComponentsInChildren<ClickObject>()[Random.Range(0, board.GetComponentsInChildren<ClickObject>().Length)].gameObject;
+            cleanPathNodes();
+         //   Debug.Log("finding a new path");
+            findPath();
+            return currentSquare;
+        }
+      //  Debug.Log(pathToGoal);
+     //   Debug.Log(pathToGoal.Count);
+        potentialSquare = pathToGoal[0].thisSquare;
+        pathToGoal.RemoveAt(0);
+        /*
+        potentialSquare = pathToGoal[0].gameObject;
+        pathToGoal.RemoveAt(0);
+        */
+        currentSquare.GetComponent<ClickObject>().setCharacter(null);
+        return potentialSquare;
+
     }
 
     // Update location performs the physical itween move to the next square, as well as setting the character of the square to this object
@@ -151,4 +167,115 @@ public class NPC : MonoBehaviour {
             return false;
         }
     }
+
+    public void findPath() {
+        openSet = new List<PathNode>();
+        closedSet = new List<PathNode>();
+        pathToGoal = new List<PathNode>();
+        GameObject container = new GameObject();
+        container.name = "containerPath";
+        container.tag = "Finish";
+        PathNode start = container.AddComponent<PathNode>();
+        start.createPathNode(currentSquare, goalPoint);
+        start.calcWeights();
+      //  Debug.Log(string.Format("we created a new start node {0} with weight {1}", start.thisSquare.name, start.fValue));
+
+        openSet = openSet.OrderBy(x => x.fValue).ToList();
+        openSet.Add(start);
+        //pathToGoal = start.findNextPath();
+        // while the list is not empty
+        while(openSet.Count != 0) {
+            PathNode current = openSet[0];
+       //     Debug.Log(string.Format("we are now on node {0}", current.thisSquare.name));
+            //Debug.Log(string.Format("path being calculated from {0} to {1}", current.thisSquare.name, goalPoint.name));
+            openSet.Remove(openSet[0]);
+
+           // Debug.Log(current.fValue);
+            current.calcWeights();
+            if(current.checkIfGoal()) {
+                //Debug.Log(string.Format("we have found goal between {0} and {1}", current.thisSquare.name, goalPoint.name));
+                pathToGoal = new List<PathNode>();
+        //        Debug.Log(string.Format("we found a path from {0} to {1}", currentSquare.name, goalPoint.name));
+                PathNode point = current;
+                while(point.parent != null) {
+          //          Debug.Log(string.Format("{0} is being added to the path to take", point.name));
+                    pathToGoal.Insert(0, point);
+                    point = point.parent;
+                }
+                // we have our path and are done
+                closedSet.RemoveRange(0, closedSet.Count);
+                openSet.RemoveRange(0, openSet.Count);
+         //       Debug.Log("we are done and moving on to NPC 95");
+                return;
+            } else {
+                handleNeighborSetting(current);
+            }
+        }
+        Debug.LogError("we have dropped out of the loop");
+    }
+
+    void handleNeighborSetting(PathNode current) {
+   //     Debug.Log("we are adding neighbors to the set");
+  //      Debug.Log(string.Format("the current node is {0}", current.name));
+        foreach(GameObject target in current.thisSquare.GetComponent<ClickObject>().neighborCubes) {
+            if(target.GetComponent<ClickObject>().getThisPath() == null) {
+          //      Debug.Log(string.Format("pathnode is null for {0}", target.name));
+                GameObject container = new GameObject();
+                container.tag = "Finish";
+                container.name = target.name + "Path";
+                PathNode targetNode = container.AddComponent<PathNode>();
+                //PathNode targetNode = target.GetComponent<PathNode>();
+      //          Debug.Log(targetNode);
+                targetNode.createPathNode(target, current, goalPoint);
+                targetNode.calcWeights();
+     //           Debug.Log(string.Format("adding {0} to open set", targetNode.thisSquare.name));
+                target.GetComponent<ClickObject>().setThisPath(targetNode);
+                openSet.Add(targetNode);
+            } else {
+                if(closedSet.Contains(target.GetComponent<ClickObject>().getThisPath())) {
+      //             Debug.Log(string.Format("closed set already contains node {0}", target.name));
+                    // then we is good
+                }
+            }
+        }
+  //      Debug.Log(string.Format("we are adding {0} to the closedset", current.thisSquare.name));
+        closedSet.Add(current);
+    }
+
+    private void cleanPathNodes() {
+        foreach(GameObject target in GameObject.FindGameObjectsWithTag("Finish")) {
+    //        Debug.Log(string.Format("Cleaning up target {0}", target.name));
+            if(target.GetComponent<PathNode>() != null) {
+                target.GetComponent<PathNode>().thisSquare.GetComponent<ClickObject>().setThisPath(null);
+            }
+            Destroy(target);
+        }
+    }
 }
+
+/*
+currentSquare.GetComponent<ClickObject>().setCharacter(null);
+if(goalPoint != null) {
+    float distance = 1000000f;
+   GameObject potentialSquare = null;
+    // this loop finds a square in the direction of the goal point
+    foreach(GameObject square in currentSquare.GetComponent<ClickObject>().neighborCubes) {
+        // the second half of this is a conditional hack so NPCs dont end up in the same square
+        if(Mathf.Abs(Vector3.Distance(square.transform.position, goalPoint.transform.position)) < distance && squareDoesntContainNPC(square)) {
+            distance = Mathf.Abs(Vector3.Distance(square.transform.position, goalPoint.transform.position));
+            // if the square contains the player
+            if(squareContainsPlayer(square)) {
+                // if it is dangerous it can be chosen 
+                if(square.GetComponent<ClickObject>().isSquareDangerous()) {
+                    potentialSquare = square;
+                }
+                // if it doesnt contain the player it can be chosen
+            } else {
+                potentialSquare = square;
+            }
+        }
+    }
+    return potentialSquare;
+}
+return null;
+*/
